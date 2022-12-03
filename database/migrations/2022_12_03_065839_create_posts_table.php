7e981400b73482b271cdb83f605f7b69dd6fa9aa
quoc le <dinhquoc0103@gmail.com>
2022-12-03 14:17:22 +0700

    create AdminPostController, AdminPostCategoryController, Post and PostCategory Model, Migaration. Connect db and migrate table to db

---
+++ database/migrations/2022_12_03_065839_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 255);
+            $table->string('thumb')->nullable();
+            $table->string('description', 255)->nullable();
+            $table->string('content')->nullable();
+            $table->tinyInteger('active')->default(0);
+            $table->bigInteger('post_category_id');
+            $table->string('slug');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
---
+++ database/migrations/2022_12_03_070045_create_post_categories_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255);
+            $table->string('thumb')->nullable();
+            $table->string('description', 255)->nullable();
+            $table->tinyInteger('active')->default(0);
+            $table->integer('level');
+            $table->bigInteger('parent_id');
+            $table->string('slug');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post_categories');
+    }
+}
